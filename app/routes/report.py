25bf3503170c35c825f06ee806fdd77b10392c83
rohit <majaganvirohit84311@gmail.com>
2025-08-31 02:47:26 +0530

    .

--- app/routes/report.py
+++ app/routes/report.py
@@ -10,29 +10,29 @@ router = APIRouter(prefix="/reports", tags=["utils"])
 async def generate_reports(request: Request):
     """
     Generate Reports:
-    process store time window start, end in local
-    extrapolate uptime and downtime
+    Process store time window start, end in local,
+    Extrapolate uptime and downtime:
         filter business hours: processed local time windows:
-            none: open 24*7 / full_day_window
         filter polls: processed local start, end, store_id
         interpolation missing business hour windows
+        none: open 24*7 / full_day_window
         calculate uptime & downtimes for each time windows
     generate report:
     :param request:
     :return: report_id
     """
-    store_ids = ["1632a271-fdc4-45ad-8dee-4383d49cde6b"]
+    store_ids = ["1632a271-fdc4-45ad-8dee-4383d49cde6b", "0a1d42a2-f6ad-4c6b-9617-375126714502", "8489c804-fdc9-4814-ac40-2094986632fe"]
     uptime_downtime_windows = []
     for store_id in store_ids:
-        # get start, stop local times
-        # in business window
         for window in ["last_hour", "last_day", "last_week"]:
+            # get start, stop local times
+            # in business window
             report_time_windows = StoreReportTimeWindow()
-            get_local_time_windows_n_days = await report_time_windows.main(store_id, window)
+            reports_time_windows = await report_time_windows.main(store_id, window)
 
             # extrapolate uptime and downtime
             extrapolate_report_windows = StoreBusinessAnalyzer()
-            uptime_downtime_window = await extrapolate_report_windows.main(store_id, get_local_time_windows_n_days, window)
+            uptime_downtime_window = await extrapolate_report_windows.main(store_id, reports_time_windows, window)
             uptime_downtime_windows.append(f"{store_id} {uptime_downtime_window}")
 
     return uptime_downtime_windows
--- app/utils/report_generator.py
+++ app/utils/report_generator.py
@@ -1,11 +1,9 @@
 import pandas as pd
 from collections import defaultdict
-from configparser import Interpolation
 from datetime import datetime, timedelta, timezone, tzinfo
-from typing import TypedDict, Dict, List, Optional
+from typing import TypedDict, Dict, List, Optional, Tuple, Any, Coroutine
 
 import pytz
-from numpy.ma.core import floor, ceil
 from tortoise.expressions import Q, Case, When, F
 
 from app.models.business_menu import StoreMenuHour
@@ -16,9 +14,12 @@ class StoreReportTimeWindow():
     """
     process report time windows
     :param store_id:
+    :param window :
     :return: total no of days, start local time, stop local time
     """
-    async def main(self, store_id, window):
+    async def main(self,
+                   store_id: str,
+                   window: str):
         start_local_time_windows = defaultdict(list)
         stop_local_time_windows = defaultdict(list)
         # time window in UTC
@@ -33,24 +34,19 @@ class StoreReportTimeWindow():
         start_local_time_windows[window].append(utc_into_local(start_utc, timezone))
         stop_local_time_windows[window].append(utc_into_local(end_utc, timezone))
 
-        # get date/time range in days
-        # total_days = (datetime.astimezone(stop_local)).day - (datetime.astimezone(start_local)).day
-        # total_days += 1 if (window == "last_hour" or window == "last_day") else 6
-
-        # return total_days, start_local_time_windows, stop_local_time_windows
         return {
-            # "week_days": total_days,
             "start_time_local": start_local_time_windows,
             "stop_time_local": stop_local_time_windows
         }
 
-    async def get_utc_window_bounds(self, time_window:str):
+    async def get_utc_window_bounds(self,
+                                    time_window:str) -> Tuple[datetime, datetime]:
         """
         get time bound window in UTC
         :param time_window:
         :return: start, stop UTC
         """
-        now_utc = datetime.now(timezone.utc) # Get the current UTC date and time
+        now_utc = datetime.now(timezone.utc) # Current datetime UTC
 
         if time_window == "last_hour":
             return (now_utc - timedelta(hours=1)), now_utc
@@ -70,7 +66,8 @@ class StoreReportTimeWindow():
             return start_of_last_week_utc, end_of_last_week_utc
 
 
-    async def get_store_timezone(self, store_id) -> StoreTimeZone:
+    async def get_store_timezone(self,
+                                 store_id: str) -> Optional[StoreTimeZone]:
         """
         get timezone string from StoreTimeZone
         :param store_id:
@@ -94,28 +91,28 @@ class StoreBusinessAnalyzer:
     """
     Filter by business hours
     Filter Polls data
-    Extrapolating uptime/downtime
+    Extrapolate uptime/downtime
     Interpolation of missing time blocks
     :param store_id, data: local time windows and total days
     :return:
     """
-    async def main(self, store_id: str, data: TimeWindowData, window: str):
-        full_day_window = lambda : None # store open 24*7
+    async def main(self,
+                   store_id: str,
+                   data: TimeWindowData,
+                   window: str) -> defaultdict[Any, list]:
         # parse data
         start_time_local = data["start_time_local"]
         stop_time_local = data["stop_time_local"]
-        # report_windows = start_time_local.keys() or stop_time_local.keys()
 
-        # for window  in report_windows:
-        total_days = 1 if window in ['last_hour', 'last_day'] else 6
         window_start_datetime_local = (start_time_local[window])[0]  # start time window
         window_stop_datetime_local = (stop_time_local[window])[0]  # stop time window
-        final_prodctivity = defaultdict(list)
+        final_productivity = defaultdict(list)
 
-        # Filter by business hours in timezone windows
-        # iterate in total days range
-        total_uptime, total_downtime = 0, 0
+        total_uptime, total_downtime = 0, 0 # Initially 0
 
+        # Filter by business hours: store_id, day lookups
+        # iterate in window days range
+        total_days = 1 if window in ['last_hour', 'last_day'] else 6
         for day in range(total_days):
             day_lookup = window_start_datetime_local.weekday() or window_stop_datetime_local.weekday() if total_days == 1 else day
             business_hours = await self.filter_business_hours(store_id, day_lookup)
@@ -153,20 +150,22 @@ class StoreBusinessAnalyzer:
                     # print(total_window_uptime, total_window_downtime)
                     total_uptime += total_window_uptime
                     total_downtime += total_window_downtime
-                    final_prodctivity[window].append(
-                        f"Up Time: {round(total_uptime / 60, 2)} Down Time: {round(total_downtime / 60, 2)}"
-                    )
-        return final_prodctivity
-
-    def str_to_time(self, time_str: str, local_timezone) -> datetime.time:
-        naive_dt = datetime.strptime(time_str, "%H:%M:%S")
-        desired_timezone = pytz.timezone(local_timezone)
-        return desired_timezone.localize(naive_dt)
+                    if window == 'last_hour' :
+                        # in minutes
+                        final_productivity[window].append(
+                            f"Up Time: {round(total_uptime, 2)} Down Time: {round(total_downtime, 2)}"
+                        )
+                    else:
+                        # in hours
+                        final_productivity[window].append(
+                            f"Up Time: {round(total_uptime / 60, 2)} Down Time: {round(total_downtime / 60, 2)}"
+                        )
+        return final_productivity
 
     async def filter_business_hours(self,
                                     store_id: str,
                                     day: int
-                                    ) -> StoreMenuHour:
+                                    ) -> list[dict[str, Any]] | dict[str, Any]:
         results = await StoreMenuHour.filter(
             Q(store_id=store_id) & Q(day_of_week=day)
         ).all().values('start_time_local', 'end_time_local', "day_of_week")
@@ -175,34 +174,33 @@ class StoreBusinessAnalyzer:
     def clip_business_hours_to_local_window(self,
                                                   window_start_datetime_local: str,
                                                   window_stop_datetime_local: str,
-                                                  business_hour_start_time_local,
-                                                  business_hour_stop_time_local):
+                                                  business_hour_start_time_local: Optional[list],
+                                                  business_hour_stop_time_local: Optional[list]) -> tuple[
+        datetime | Any, datetime | Any]:
         start_stop_local_tz = window_start_datetime_local.tzinfo and window_stop_datetime_local.tzinfo
         start_window_local_date = window_start_datetime_local.date()
         combined_window_starts_naive = datetime.strptime(
             f'{start_window_local_date} {business_hour_start_time_local[0]}', '%Y-%m-%d %H:%M:%S')
         window_starts = start_stop_local_tz.localize(
-            combined_window_starts_naive) if not combined_window_starts_naive.tzinfo else combined_window_starts_naive.astimezone(
-            start_stop_local_tz)
+            combined_window_starts_naive) if not combined_window_starts_naive.tzinfo else (
+            combined_window_starts_naive.astimezone(start_stop_local_tz))
 
         stop_window_local_date = window_stop_datetime_local.date()
         combined_window_ends_naive = datetime.strptime(
             f'{stop_window_local_date} {business_hour_stop_time_local[0]}', '%Y-%m-%d %H:%M:%S')
         window_ends = start_stop_local_tz.localize(
-            combined_window_ends_naive) if not combined_window_ends_naive.tzinfo else combined_window_ends_naive.astimezone(
-            start_stop_local_tz)
+            combined_window_ends_naive) if not combined_window_ends_naive.tzinfo else (
+            combined_window_ends_naive.astimezone(start_stop_local_tz))
 
         return window_starts, window_ends
-        # window_starts = max(business_hour_start_time_local[0], window_start_datetime_local)
-        # window_ends = max(business_hour_stop_time_local[0], window_stop_datetime_local)
 
     async def get_store_polls_status(self,
                                      store_id: str,
                                      window_start_time: Optional[datetime] = None,
                                      window_end_time: Optional[datetime] = None
-                                     ) -> StorePolls:
+                                     ) -> list[dict[str, Any]] | dict[str, Any]:
         """
-        filter store Active polls in UTC time
+        filter store Active polls in UTC timezone
         :param store_id:
         :param window_start_time:
         :param window_end_time:
@@ -211,7 +209,6 @@ class StoreBusinessAnalyzer:
 
         results = await (StorePolls.filter(
             Q(store_id=store_id) &
-             # Q(status=True)) &
             (Q(timestamp_utc__gte=window_start_time) &
              Q(timestamp_utc__lte=window_end_time))
         ).values("store_id", "timestamp_utc", "status"))
@@ -242,13 +239,16 @@ class InterpolationWindow:
         Status spans multiple days	Don't carry over — each day is treated independently
         No polls at all	Assume 100% uptime
     """
-    def __init__(self, polls, window_start: datetime, window_end: datetime):
+    def __init__(self,
+                 polls: list[dict[str, Any]] | dict[str, Any],
+                 window_start: datetime,
+                 window_end: datetime
+                 ):
         self.polls = polls
         self.window_start = window_start
         self.window_end = window_end
 
-    def work_flow(self):
-        # Todo: add 100% uptime if polls None
+    def work_flow(self) -> tuple[int, int]:
         df = pd.DataFrame(self.polls)
 
         local_tz = self.window_start.tzinfo or self.window_end.tzinfo
@@ -261,7 +261,7 @@ class InterpolationWindow:
 
         intervals = []
         prev_time = self.window_start
-        prev_status = True # inactive
+        prev_status = False # inactive
 
         for _, row in df.iterrows():
             current_time = row['timestamp']
